7d56adc5fc56452480df76ae19a19486ef8d6077
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-01-24 17:23:47 -0500

    Update Mouse_Cursor_Size_Adjust.bat
    Better readability

--- Mouse_Cursor_Size_Adjust.bat
+++ Mouse_Cursor_Size_Adjust.bat
@@ -1,7 +1,7 @@
-title Mouse_Cursor_Size_Adjust
+title Mouse_Cursor_Size_Adjust REM So Enterinput/END files can interact with CLI
 @echo off
 
-set targetProcess=
+set targetProcess=swtor.exe
 set timerActivated?=false
 set Input=EnterInput.py
 set InputLast=EnterInputEND.py
@@ -14,14 +14,18 @@ REM Start program with :MonitorTargetProgram section.
 REM Detect if the task has ended
 :Executor
     set targetEnd=false
+
 :AltLoop
     tasklist | findstr /i %targetProcess% > nul
+    
     if %errorlevel% neq 0 (
         REM If False, start separate program that sets cursor size back to default, else go into the input standby.
         set targetEnd=true
+
         start CursorSizeAdjustDefault.py
         timeout /t 3.5 /nobreak
         exit
+
     ) else (
         REM Start separate process that waits for targetProcess to end, restarts the loop when it has done so.
         start /b EnterInputEND.py
@@ -32,12 +36,15 @@ REM Detect if the task has ended
 REM Detect if the program has started
 :MonitorTargetProgram
     set foundTarget=false
+
 :loop
     tasklist | findstr /i %targetProcess% > nul
+
     if %errorlevel% equ 0 (
         REM If True, goto the label
         set foundTarget=true
         goto :CreateCursor
+
     ) else (
         REM Open separate program that detects when targetProcess is found
         start /b EnterInput.py
@@ -47,11 +54,13 @@ REM Detect if the program has started
     )
 
 :CreateCursor
-REM Search Registry for the default size of cursor, if the cursor size is default, 
-REM then separate program launches to increase cursor size in control panel, and continues to next section. 
+    REM Search Registry for the default size of cursor, if the cursor size is default, 
+    REM then separate program launches to increase cursor size in control panel, and continue to next section. 
     reg query "HKCU\Control Panel\Cursors" /v CursorBaseSize /t REG_DWORD | find "0x20" && set "modificationAmount=true" || set "modificationAmount=false"
+
     if %errorlevel% neq 0 (
         exit /b
+        
     ) else (
         start CursorSizeAdjust.py
         timeout /t 4 /nobreak
@@ -59,10 +68,12 @@ REM then separate program launches to increase cursor size in control panel, and
     )
 
 REM Conditional determines if this section has already been executed, if true, then the "else" statement is ran
+
 if "%timerActivated?%"=="false" (
     REM Failsafe in case the process is not already running, once process is detected, continues to go back to the :loop section.
     start /b EnterInput.py
     set /p input=Waiting for program to start...
+    
 ) else (
     REM If this section was executed at least once, then wait 3 seconds and continue to the :Executor section.
     timeout /t 3 /nobreak
