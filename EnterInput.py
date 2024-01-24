bd81d144337633948f1d94f2d21fe986028e552d
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-01-24 16:23:00 -0500

    Update EnterInput.py
    Organized for better readability, imported os to start AutoMove.py

--- EnterInput.py
+++ EnterInput.py
@@ -2,21 +2,31 @@ import pywinauto.keyboard as keyboard
 import time
 from pywinauto import Application
 import subprocess
-#When batch file opens, access the window and set focus on the input field
+import os
+
+# When batch file opens, access the window and set focus on the input field
 app = Application(backend="uia").connect(title="Mouse_Cursor_Size_Adjust")
+
 batch_window = app.window(title="Mouse_Cursor_Size_Adjust")
+
 user_input_field = batch_window["Text Area"]
 
-targetProcess = ""
+targetProcess = "swtor.exe"
 
 # Check if the target process is present in the output
 while True:
-    #Loop the output to ensure the activation of the process is accurately detected
+    # Loop the output to ensure the activation of the process is accurately detected
     output = subprocess.run(["tasklist"], capture_output=True, text=True)
+
     if targetProcess.lower() in output.stdout.lower():
         user_input_field.set_focus()
+
         keyboard.SendKeys('{ENTER}')
-        break   
+        time.sleep(1)
+
+        os.startfile("AutoMove.py")
+        break
+
     else:
         print('Process not found...')
-        time.sleep(1)  # Add a small delay to prevent excessive CPU usage
+        time.sleep(1)
