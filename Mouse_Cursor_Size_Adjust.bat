cb3cb1279f017ef8f72fdd3d4f873e5896d94f58
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-01-24 17:26:40 -0500

    Update Mouse_Cursor_Size_Adjust.bat
    Even better readability

--- Mouse_Cursor_Size_Adjust.bat
+++ Mouse_Cursor_Size_Adjust.bat
@@ -15,43 +15,43 @@ REM Detect if the task has ended
 :Executor
     set targetEnd=false
 
-:AltLoop
-    tasklist | findstr /i %targetProcess% > nul
+    :AltLoop
+        tasklist | findstr /i %targetProcess% > nul
+        
+        if %errorlevel% neq 0 (
+            REM If False, start separate program that sets cursor size back to default, else go into the input standby.
+            set targetEnd=true
     
-    if %errorlevel% neq 0 (
-        REM If False, start separate program that sets cursor size back to default, else go into the input standby.
-        set targetEnd=true
-
-        start CursorSizeAdjustDefault.py
-        timeout /t 3.5 /nobreak
-        exit
-
-    ) else (
-        REM Start separate process that waits for targetProcess to end, restarts the loop when it has done so.
-        start /b EnterInputEND.py
-        set /p input=Waiting for program to end...
-        goto :AltLoop 
-    )
+            start CursorSizeAdjustDefault.py
+            timeout /t 3.5 /nobreak
+            exit
+    
+        ) else (
+            REM Start separate process that waits for targetProcess to end, restarts the loop when it has done so.
+            start /b EnterInputEND.py
+            set /p input=Waiting for program to end...
+            goto :AltLoop 
+        )
 
 REM Detect if the program has started
 :MonitorTargetProgram
     set foundTarget=false
 
-:loop
-    tasklist | findstr /i %targetProcess% > nul
-
-    if %errorlevel% equ 0 (
-        REM If True, goto the label
-        set foundTarget=true
-        goto :CreateCursor
-
-    ) else (
-        REM Open separate program that detects when targetProcess is found
-        start /b EnterInput.py
-        set /p input=Waiting for program to start...
-        timeout /t 2 /nobreak
-        goto :loop
-    )
+    :loop
+        tasklist | findstr /i %targetProcess% > nul
+    
+        if %errorlevel% equ 0 (
+            REM If True, goto the label
+            set foundTarget=true
+            goto :CreateCursor
+    
+        ) else (
+            REM Open separate program that detects when targetProcess is found
+            start /b EnterInput.py
+            set /p input=Waiting for program to start...
+            timeout /t 2 /nobreak
+            goto :loop
+        )
 
 :CreateCursor
     REM Search Registry for the default size of cursor, if the cursor size is default, 
@@ -73,7 +73,7 @@ if "%timerActivated?%"=="false" (
     REM Failsafe in case the process is not already running, once process is detected, continues to go back to the :loop section.
     start /b EnterInput.py
     set /p input=Waiting for program to start...
-    
+
 ) else (
     REM If this section was executed at least once, then wait 3 seconds and continue to the :Executor section.
     timeout /t 3 /nobreak
