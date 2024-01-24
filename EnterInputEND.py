22761986a811b14c9d359e330192c8704e429d0f
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-01-24 16:30:22 -0500

    Update EnterInputEND.py
    Organized for better readability

--- EnterInputEND.py
+++ EnterInputEND.py
@@ -2,21 +2,37 @@ import pywinauto.keyboard as keyboard
 from pywinauto import Application
 import subprocess
 import time
+
 #When batch file opens, access the window and set focus on the input field
 app = Application(backend='uia').connect(title='Mouse_Cursor_Size_Adjust')
+
 batch_window = app.window(title='Mouse_Cursor_Size_Adjust')
+
+if batch_window == False:
+    while True:
+        print('Reconnecting...')
+        time.sleep(1)
+
+        Application(backend='uia').connect(title='Mouse_Cursor_Size_Adjust')
+
+        if batch_window == True:
+            break
+
 user_input_field = batch_window['Text Area']
 
-targetProcess = ""
+targetProcess = "swtor.exe" # Application executable
 
-# Check if the target process is NOT present in the output
+# Check if the target process is NOT present in the output/task list
 while True:
     #Loop the output to ensure the ending of the process is accurately detected
     output = subprocess.run(['tasklist'], capture_output=True, text=True)
+
     if targetProcess.lower() not in output.stdout.lower():
         user_input_field.set_focus()
+
         keyboard.SendKeys('{ENTER}')
         break
+
     else:
         print('Process still running...')
-        time.sleep(1)  # Add a small delay to prevent excessive CPU usage
+        time.sleep(1)
