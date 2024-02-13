ecc250ac4c600122f8057ae45d6c4da555a25c51
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-02-12 20:03:36 -0500

    Update Launcher_check.py
    Fixed issues in logic

--- Launcher_check.py
+++ Launcher_check.py
@@ -3,6 +3,7 @@ import pyautogui
 import os
 import subprocess
 import threading
+import time
 import pygetwindow as gw
 from AutoMove_shared_variables import v, update_bool
 # Some functions are no longer used due to changes in code
@@ -11,11 +12,11 @@ from initial_execution_funcs import subproces, init_var, var_false, read, write,
 LAUNCHER_TITLE = "Star Wars™: The Old Republic™" # Task title of launcher window
 
 # Pointers to files
-FILE_PATH = [os.path.join(os.path.dirname(os.path.abspath(__file__)), "Launcher_check.py"), 
+FILE_PATH = [ 
              os.path.join(os.path.dirname(os.path.abspath(__file__)), "text_field_input_detection.py"),
              os.path.join(os.path.dirname(os.path.abspath(__file__)), "LauncherCreds.py"),
-
-             os.path.join(os.path.dirname(os.path.abspath(__file__)), "error_thrown.txt")
+             os.path.join(os.path.dirname(os.path.abspath(__file__)), "error_thrown.txt"),
+             os.path.join(os.path.dirname(os.path.abspath(__file__)), "Launcher_check.py")
              ]
 
 if __name__ == "__main__":
@@ -34,17 +35,17 @@ if __name__ == "__main__":
 
         def update_int(self): # Gets current number in txt and increments by 1
             try:
-                integer = read(FILE_PATH[3])
-                updated_int = string_to_int(FILE_PATH[3], integer)
+                integer = read(FILE_PATH[2])
+                updated_int = string_to_int(FILE_PATH[2], integer)
                 return updated_int
 
             except FileNotFoundError as e:
                 print("Error: {e}")
-                write(FILE_PATH[3], '')
-                write(FILE_PATH[3], '0')
+                write(FILE_PATH[2], '')
+                write(FILE_PATH[2], '0')
 
-                integer = read(FILE_PATH[3])
-                updated_int = string_to_int(FILE_PATH[3], integer)
+                integer = read(FILE_PATH[2])
+                updated_int = string_to_int(FILE_PATH[2], integer)
                 return updated_int
             
         def is_window_active(self):
@@ -69,6 +70,8 @@ if __name__ == "__main__":
             active_thread = threading.Thread(target=self.is_window_active)
             active_thread.start()
 
+            global alrdy_executed
+            alrdy_executed = False
             while True:
                 # Find the target window in task list
                 self.launcher_window = win32gui.FindWindow(None, LAUNCHER_TITLE)
@@ -76,81 +79,79 @@ if __name__ == "__main__":
 
                 if self.launcher_window:
                     print("Launcher found")
-                    alrdy_executed = False
 
-                    for i in range(40):
-                        print(i)
-                        
-                        if i == 39:
-                            os.system("taskkill /IM python.exe /F")
-                        
-                        try:
-                        # Set focus to the launcher window, then retrieve the ID of the active window
-                            launcher_window = gw.getWindowsWithTitle(LAUNCHER_TITLE)
-                            self.event.wait()
-                            self.event.clear() # Signals that the active_window has been updated
+                    try:
+                    # Set focus to the launcher window, then retrieve the ID of the active window
+                        launcher_window = gw.getWindowsWithTitle(LAUNCHER_TITLE)
+                        self.event.wait()
+                        self.event.clear() # Signals that the active_window has been updated
 
-                            if not active_window: # If the target window is not in focus
-                                v('handle', False)
-                                print("activating")
-                                self.timeout.wait(0.15)
+                        if not active_window: # If the target window is not in focus
+                            alrdy_executed = True
+                            print("activating")
 
-                                launcher_window[0].restore() # In case window is minimized
-                                launcher_window[0].activate() # Activate the first window in the list
-                                v('handle', True)
+                            launcher_window[0].restore() # In case window is minimized
+                            launcher_window[0].activate() # Activate the first window in the list
 
-                                # Get current mouse position, then move mouse over selection field
+                            # Get current mouse position, then move mouse over selection field
+                            original_mouse_position = pyautogui.position()
+                            pyautogui.moveTo(self.target_x, self.target_y, duration=0)
+                            
+
+                            speed_modified = update_bool("speed_modified") # See: mouse_speed.py
+
+                            if not speed_modified: # Only open process if the mouse sensitivity is not currently modified
+                                subprocess.Popen(["python.exe", FILE_PATH[0]], shell=True) # text_field_input_detection.py
+
+                            while True:
+                                mouse_over_field = update_bool('mouse_over_field') # See: text_field_input_detection.py
+                                
+                                if mouse_over_field:
+                                    pyautogui.click()
+                                    pyautogui.moveTo(original_mouse_position)
+                                    subprocess.Popen(["python.exe", FILE_PATH[1]], shell=True) # "LauncherCreds.py"
+                                    break
+
+                        else: # When the launcher is within focus, this block is executed
+                            """
+                            When launcher first starts, execution of this program interrupts focus to launcher, so 
+                            immediately refocus with mouse. """
+                            if not alrdy_executed: # This if block is only executed once
                                 original_mouse_position = pyautogui.position()
+                                
                                 pyautogui.moveTo(self.target_x, self.target_y, duration=0)
-
-                                speed_modified = update_bool("speed_modified") # See: mouse_speed.py
-
-                                if not speed_modified: # Only open process if the mouse sensitivity is not currently modified
-                                    subprocess.Popen(["python.exe", FILE_PATH[1]], shell=True) # text_field_input_detection.py
+                                subprocess.Popen(["python.exe", FILE_PATH[0]], shell=True) # text_field_input_detection.py
 
                                 while True:
-                                    mouse_over_field = update_bool('mouse_over_field') # See: text_field_input_detection.py
+                                    mouse_over_field = update_bool('mouse_over_field')
+
                                     if mouse_over_field:
                                         pyautogui.click()
                                         pyautogui.moveTo(original_mouse_position)
                                         break
 
-                            else: # When the launcher is within focus, this block is executed
-                                """
-                                When launcher first starts, execution of this program interrupts focus to launcher, so 
-                                immediately refocus with mouse. """
-                                if not alrdy_executed: # This if block is only executed once
-                                    original_mouse_position = pyautogui.position()
-                                    pyautogui.moveTo(self.target_x, self.target_y, duration=0)
-                                    subprocess.Popen(["python.exe", FILE_PATH[1]], shell=True) # text_field_input_detection.py
-
-                                    while True:
-                                        mouse_over_field = update_bool('mouse_over_field')
-
-                                        if mouse_over_field:
-                                            pyautogui.click()
-                                            pyautogui.moveTo(original_mouse_position)
-                                            break
-
-                                    subprocess.Popen(["python.exe", FILE_PATH[2]], shell=True) # "LauncherCreds.py"
-                                    v('handle', True)
-                                    alrdy_executed = True
-
+                                subprocess.Popen(["python.exe", FILE_PATH[1]], shell=True) # "LauncherCreds.py"
+                                alrdy_executed = True
                                 self.timeout.wait(0.3)
 
-                        except gw.PyGetWindowException as e:
-                            print(f"An error occurred: {e}")
-                            print("wait for sleep:")
-                            self.timeout.wait(0.2)
+                        self.times += 1
+                        if self.times == 8:
+                            os.system("taskkill /IM python.exe /F")
 
-                            v('handle', False)
-                            error_thrown = self.update_int()
+                    except gw.PyGetWindowException as e:
+                        print(f"An error occurred: {e}")
+                        print("wait for sleep:")
+                        self.timeout.wait(1)
 
-                            if error_thrown >= 10: # Set amount of errors to be allowed
-                                print('Max errors reached, exiting...')
-                                int_reset() # Reset error_thrown to 0
+                        subprocess.Popen(["python.exe", FILE_PATH[3]], shell=True)
 
-                                os.system("taskkill /IM python.exe /F") # Force kill all instances
+                        error_thrown = self.update_int()
+
+                        if error_thrown >= 10: # Set amount of errors to be allowed
+                            print('Max errors reached, exiting...')
+                            int_reset() # Reset error_thrown to 0
+
+                            os.system("taskkill /IM python.exe /F") # Force kill all instances
             
                 else:
                     print("Launcher not running...")
