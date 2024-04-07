5329b5cc371b7bc9a1c073308d174fa6e791dbdf
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-06 22:50:26 -0400

    Add function to find Settings window
    Contains error handling and extra logic to determine some character output

--- ModifyCursorSize/Source.cpp
+++ ModifyCursorSize/Source.cpp
@@ -2,8 +2,48 @@
 #include <iostream>
 #include <windows.h>
 #include <UIAutomation.h>
+#include <UIAutomationCore.h>
 
+int getSettingsHandle()
+{
+	bool errorThrown = false;
+	for (int i = 0; i <= 5; i++) // Limit thrown errors
+	{
+		HWND settingsHwnd = FindWindowW(L"ApplicationFrameWindow", L"Settings");
+		if (!settingsHwnd)
+		{
+			std::cerr << "Error: Window not found, attempting to resolve..." << std::endl;
+			ShellExecuteW(NULL, L"Open", L"ms-settings:", NULL, NULL, SW_NORMAL);
+			errorThrown = true;
+		}
+		if (i == 4)
+		{
+			return 1;
+		}
+		else if ( errorThrown && settingsHwnd)
+		{
+			std::cout << "Error resolved!" << std::endl;
+			break; // If error resolves, continue with rest of code
+		}
+		else if (settingsHwnd)
+		{
+			std::cout << "Settings handle retrieved" << std::endl;
+			break; // If no error thrown, continue with rest of code
+		}
+	}
+	return 0; // Exit function
+}
 
-int main() {
-	ShellExecuteW(NULL, L"Open", L"ms-settings", NULL, NULL, SW_NORMAL);
+int main() 
+{
+	/* Open the settings window, navigate to accessibility, Mouse pointer, and focus into pointer
+	size slider */
+	ShellExecuteW(NULL, L"Open", L"ms-settings:", NULL, NULL, SW_NORMAL);
+
+	// Get Setting's top window element
+	getSettingsHandle();
+
+	std::cout << "Continuing with code...";
+
+	Sleep(5000);
 }
