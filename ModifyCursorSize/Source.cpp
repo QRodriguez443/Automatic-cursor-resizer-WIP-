dde8e7d0e33b3eadc3401b775e746a62213c449c
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-08 14:08:40 -0400

    Update Source.cpp 3
    Added block at start of getSettingsHandle() to check if the program is already running.

--- ModifyCursorSize/Source.cpp
+++ ModifyCursorSize/Source.cpp
@@ -8,36 +8,46 @@
 
 HWND getSettingsHandle() // Executes and retrieves Settings window handle
 {
-	bool errorThrown = false;
-	for (int i = 0; i <= 5; i++) // Limit thrown errors
+	//Check if Settings is already running
+	HWND settingsHwnd = FindWindowW(L"ApplicationFrameWindow", L"Settings");
+	if (settingsHwnd != NULL)
 	{
-		ShellExecuteW(NULL, L"Open", L"ms-settings:", NULL, NULL, SW_NORMAL);
+		std::cout << "Window already open, Settings handle retrieved" << std::endl;
+		return settingsHwnd;
+	}
+	else
+	{
+		bool errorThrown = false;
+		for (int i = 0; i <= 5; i++) // Limit thrown errors
+		{
+			ShellExecuteW(NULL, L"Open", L"ms-settings:", NULL, NULL, SW_NORMAL);
 
-		HWND settingsHwnd = FindWindowW(L"ApplicationFrameWindow", L"Settings");
-		if (settingsHwnd == NULL)
-		{
-			std::cerr << "Error: Window not found, attempting to resolve..." << std::endl;
-			errorThrown = true;
-		}
-		if (i == 5)
-		{
-			try {
-			    throw std::runtime_error("Too many errors occurred");
+			HWND settingsHwnd = FindWindowW(L"ApplicationFrameWindow", L"Settings");
+			if (settingsHwnd == NULL)
+			{
+				std::cerr << "Error: Window not found, attempting to resolve..." << std::endl;
+				errorThrown = true;
 			}
-			catch (const std::runtime_error& e) {
-				// Handle the exception
-				std::cerr << "Caught a runtime error: " << e.what() << '\n';
+			if (i == 5)
+			{
+				try {
+					throw std::runtime_error("Too many errors occurred");
+				}
+				catch (const std::runtime_error& e) {
+					// Handle the exception
+					std::cerr << "Caught a runtime error: " << e.what() << '\n';
+				}
 			}
-		}
-		else if ( errorThrown && settingsHwnd)
-		{
-			std::cout << "Error resolved!" << " Settings handle retrieved" << std::endl;
-			return settingsHwnd;
-		}
-		else if (settingsHwnd)
-		{
-			std::cout << "Settings handle retrieved" << std::endl;
-			return settingsHwnd;
+			else if (errorThrown && settingsHwnd)
+			{
+				std::cout << "Error resolved!" << " Settings handle retrieved" << std::endl;
+				return settingsHwnd;
+			}
+			else if (settingsHwnd)
+			{
+				std::cout << "Settings handle retrieved" << std::endl;
+				return settingsHwnd;
+			}
 		}
 	}
 }
