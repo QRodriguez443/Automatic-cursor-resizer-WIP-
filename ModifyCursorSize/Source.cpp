51e16db5c485c03821ad8177c5c6d3d5757fe11a
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-12 16:39:08 -0400

    Update Source.cpp
    Create new function to initialize UIAutomation, and return the settings element: InitUIA().

--- ModifyCursorSize/Source.cpp
+++ ModifyCursorSize/Source.cpp
@@ -1,4 +1,3 @@
-#define OEMRESOURCE
 #include "RetrieveWindowHandle.h"
 #include <UIAutomation.h>
 #include <iostream>
@@ -6,12 +5,30 @@
 #pragma comment(lib, "oleaut32.lib")
 #pragma comment(lib, "uiautomationcore.lib")
 
+// Initialize UIAutomation and retrieve settings element
+IUIAutomationElement* InitUIA(HWND settingsHwnd)
+{
+	IUIAutomation* root = nullptr;
+	CoInitialize(NULL);
+	CoCreateInstance(__uuidof(CUIAutomation), NULL, CLSCTX_INPROC_SERVER, __uuidof(IUIAutomation), (void**)&root);
+
+	IUIAutomationElement* settingsElement;
+	root->ElementFromHandle(settingsHwnd, &settingsElement);
+	if (!settingsElement)
+	{
+		std::cerr << "Error getting settingsElement" << '\n';
+	}
+	return settingsElement;
+}
+
 int main() 
 {
 	/* Open the settings window, navigate to accessibility, Mouse pointer, and focus into pointer
 	size slider */
 
-	// Get Setting's top window element
+	// Get Setting's bottom window element
 	HWND settingsHwnd = GetSettingsHandle();
-
+	IUIAutomationElement* settingsElement = InitUIA(settingsHwnd);
+	
+	// Get "pane" element
 }
