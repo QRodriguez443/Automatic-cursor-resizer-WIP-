61275363530bafce25fcf7b6f9bd0d2377d59885
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-16 16:26:04 -0400

    Update Source.cpp
    Removed unnecessary pragmatics, replaced NULLs with pointers, added structure for returning settingsElement and root from InitUIA(); added error handling, added call to function: FindAccessibilityButton()

--- ModifyCursorSize/Source.cpp
+++ ModifyCursorSize/Source.cpp
@@ -1,24 +1,56 @@
 #include "RetrieveWindowHandle.h"
+#include "FindAccessibility.h"
 #include <UIAutomation.h>
 #include <iostream>
-#pragma comment(lib, "ole32.lib")
-#pragma comment(lib, "oleaut32.lib")
-#pragma comment(lib, "uiautomationcore.lib")
+
+struct AInitElements
+{
+	IUIAutomationElement* settingsElement;
+	IUIAutomation* root;
+};
 
 // Initialize UIAutomation and retrieve settings element
-IUIAutomationElement* InitUIA(HWND settingsHwnd)
+AInitElements InitUIA(HWND settingsHwnd)
 {
 	IUIAutomation* root = nullptr;
-	CoInitialize(NULL);
-	CoCreateInstance(__uuidof(CUIAutomation), NULL, CLSCTX_INPROC_SERVER, __uuidof(IUIAutomation), (void**)&root);
-
-	IUIAutomationElement* settingsElement;
-	root->ElementFromHandle(settingsHwnd, &settingsElement);
-	if (!settingsElement)
+	CoInitialize(nullptr);
+	HRESULT result = CoCreateInstance(__uuidof(CUIAutomation), nullptr, CLSCTX_INPROC_SERVER, __uuidof(IUIAutomation), (void**)&root);
+	if (FAILED(result))
 	{
-		std::cerr << "Error getting settingsElement" << '\n';
+		throw std::runtime_error("Error: Init failed");
 	}
-	return settingsElement;
+
+	bool errorThrown = false;
+	IUIAutomationElement* settingsElement = nullptr;
+
+	for (int i = 0; i <= 5; i++) // Limit thrown errors
+	{
+		root->ElementFromHandle(settingsHwnd, &settingsElement);
+		if (!settingsElement)
+		{
+			std::cerr << "Error getting settingsElement" << '\n';
+			errorThrown = true;
+		}
+		else if (i == 5)
+		{
+			throw std::runtime_error("Too many errors occurred");
+		}
+		else
+		{
+			if (errorThrown)
+			{
+				std::wcout << "Error resolved..." << std::endl;
+			}
+			std::wcout << "Bottom settings element found: " << settingsElement << std::endl;
+			break;
+		}
+	}
+
+	AInitElements AIE;
+	AIE.settingsElement = settingsElement;
+	AIE.root = root;
+
+	return AIE;
 }
 
 int main() 
@@ -28,7 +60,10 @@ int main()
 
 	// Get Setting's bottom window element
 	HWND settingsHwnd = GetSettingsHandle();
-	IUIAutomationElement* settingsElement = InitUIA(settingsHwnd);
+	AInitElements AIE = InitUIA(settingsHwnd);
 	
-	// Get "pane" element
+	// Find Accessibility list item: 
+	FindAccessibilityButton(AIE.settingsElement, AIE.root);
+	// 3. Accessibility: AID: SettingsPageGroupEaseOfAccess
+
 }
