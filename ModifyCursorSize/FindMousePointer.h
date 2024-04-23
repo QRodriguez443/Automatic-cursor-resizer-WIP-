d765dbdc226d82b1f44496ab8d8f82e9808f1887
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-22 22:53:59 -0400

    FindMousePointer.h

---
+++ ModifyCursorSize/FindMousePointer.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef FIND_MOUSE_POINTER_H
+#define FIND_MOUSE_POINTER_H
+
+#include <UIAutomation.h>
+
+void FindMousePointer(IUIAutomationElement* settingsElement, IUIAutomation* root);
+
+#endif
