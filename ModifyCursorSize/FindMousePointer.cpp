a89b10f764096edfec3bee79efa2e5bc1b297f26
QRodriguez443 <141974332+QRodriguez443@users.noreply.github.com>
2024-04-22 22:46:06 -0400

    Update FindMousePointer.cpp
    Included CreateCondition(); new function: GetVisionElement => IUIAutomationElement* visionElement; SysFreeStrings no longer necessary; blocks for defining a condition replaced with CreateCondition().

--- ModifyCursorSize/FindMousePointer.cpp
+++ ModifyCursorSize/FindMousePointer.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <UIAutomation.h>
+#include "CreateCondition.h"
 
 void FindMousePointer(IUIAutomationElement* settingsElement, IUIAutomation* root)
 {
 	// Define the class name to search for
-	VARIANT pClass;
-	pClass.vt = VT_BSTR;
-	pClass.bstrVal = SysAllocString(L"ListView");
-	IUIAutomationCondition* condition;
-	root->CreatePropertyCondition(UIA_ClassNamePropertyId, pClass, &condition);
+	IUIAutomationCondition* condition = CreateCondition(root, L"ListView", UIA_ClassNamePropertyId);
 
 	// Search children in settings element for the element with specified class name
 	IUIAutomationElement* listItem;
 	settingsElement->FindFirst(TreeScope_Children, condition, &listItem);
 	if (!listItem)
 	{
-		SysFreeString(pClass.bstrVal);
 		condition->Release();
 		throw std::runtime_error("Error finding list element");
 	}
-	SysFreeString(pClass.bstrVal);
 	condition->Release();
+
+	IUIAutomationElement* visionElement = GetVisionElement(listItem, root);
+}
+
+IUIAutomationElement* GetVisionElement(IUIAutomationElement* listItem, IUIAutomation* root)
+{
+	IUIAutomationCondition* condition = CreateCondition(root, L"Vision", UIA_NamePropertyId);
+
+	IUIAutomationElement* visionElement;
+	listItem->FindFirst(TreeScope_Children, condition, &visionElement);
+	if (!visionElement)
+	{
+		throw std::runtime_error("Error getting Vision element")
+	}
+	return visionElement;
 }
